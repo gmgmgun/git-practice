7ed67d9511ba872ed1a04fb1c87e092a7391b418
gmgmgun <gmgmgun@gmail.com>
2023-02-02 18:44:53 +0900

    Add: Main component

--- Frontend/git-test/src/App.js
+++ Frontend/git-test/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function App() {
   return (
     <div className="app">
       <h1>App</h1>
+      <h1>App2</h1>
+      <h1>App3</h1>
     </div>
   );
 }
